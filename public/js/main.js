8ee9c7314c885ca0c5532919a8993354f9cf8498
Roshith R <r.roshith@gmail.com>
2021-01-21 12:58:52 +0530

    clipping and putting the image(tile) on the canvas

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,16 @@
-const canvas = document.getElementById("screen")
-const context = canvas.getContext("2d")
-context.fillStyle = "#3178c6"
-context.fillRect(0, 0, canvas.width / 2, canvas.height / 2)
+const loadImage = (url) =>
+  new Promise((resolve, reject) => {
+    const image = new Image() //image instances
+    image.addEventListener('load', () => {
+      resolve(image)
+    })
+    image.src = url
+    image.alt = 'tiles'
+  })
+const canvas = document.getElementById('screen')
+const context = canvas.getContext('2d')
+context.fillRect(0, 0, 50, 50)
+
+loadImage('/img/tiles.png').then((img) =>
+  context.drawImage(img, 0, 0, 16, 16, 32, 32, 16, 16)
+)
