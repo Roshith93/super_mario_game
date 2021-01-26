052a562b1850087a69681337a9eb5cd15c1adeaf
Roshith R <r.roshith@gmail.com>
2021-01-26 17:19:54 +0530

    Promised based call enabled for loading image

--- public/js/main.js
+++ public/js/main.js
@@ -10,22 +10,20 @@ function drawBackground(background, context, sprite) {
     }
   })
 }
-
+const loadBackgroundSprites = () => {
+  return loadImage('/img/tiles.png').then((img) => {
+    const sprite = new SpriteSheet(img, 16, 16)
+    sprite.define('ground', 0, 0)
+    sprite.define('sky', 3, 23)
+    return sprite
+  })
+}
 const canvas = document.getElementById('screen')
 const context = canvas.getContext('2d')
 
-loadImage('/img/tiles.png').then((img) => {
-  const sprite = new SpriteSheet(img, 16, 16)
-  sprite.define('ground', 0, 0)
-  sprite.define('sky', 3, 23)
-  loadLevel('1-1').then((level) =>
-  level.backgrounds.forEach(background => {
-    drawBackground(background, context, sprite)
-  })
-  )
-  for (let x = 0; x < 25; ++x) {
-    for (let y = 12; y < 14; ++y) {
-      sprite.drawTile('ground', context, x, y)
-    }
-  }
-})
+Promise.all([loadBackgroundSprites(), loadLevel('1-1')]).then(
+  ([sprite, level]) =>
+    level.backgrounds.forEach((background) => {
+      drawBackground(background, context, sprite)
+    })
+)
